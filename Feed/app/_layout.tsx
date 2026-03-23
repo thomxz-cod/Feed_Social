b267321dae00962614d88de6a252af69e5900fab
thomxz-cod <thomazerick35@gmail.com>
2026-03-23 13:42:59 -0300

    create

---
+++ Feed/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "@/hooks/useTheme";
+import { Stack } from "expo-router";
+
+
+
+export default function RootLayout() {
+  return (
+      <ThemeProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </ThemeProvider>
+  )
+}
